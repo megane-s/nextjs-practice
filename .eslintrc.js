08a9fd10ecbdb032874ba71ce1d496528783e1f2
TBSten <programmingcafeteria@gmail.com>
2023-11-13 15:53:47 +0900

    minshumi-frontendのセットアップを適用

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // (A)
+    'plugin:@typescript-eslint/recommended-requiring-type-checking', // (B)
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  root: true,
+  rules: {
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+}
---
+++ .github/ISSUE_TEMPLATE/空のissue.md
@@ -0,0 +1,10 @@
+---
+name: 空のissue
+about: いずれにも該当しないとき
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+<!-- 適切な label を忘れずに設定してください。 -->
