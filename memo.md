59f1d318ce943e3b57436d221a618f0f78e93051
TBSten <programmingcafeteria@gmail.com>
2023-11-14 15:50:43 +0900

    実践練習用に削除

--- memo.md
+++
@@ -1,4 +0,0 @@
-
-mock server 起動
-yarn json-server --watch ./db.json -p 3001
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,6 @@
-import { getTrends } from "@/art/getTrends"
-import { AddArtButton } from "./AddArtButton"
-
 export default async function Home() {
-  const trends = await getTrends()
   return (
     <div>
-      {trends.map(art =>
-        <div key={art.id}>
-          {art.title}
-        </div>
-      )}
-      <AddArtButton />
     </div>
   )
 }
